3d04b8272c9efa16cd99839d08a5af9c82318a65
Dattatray patil <dattapatil862002@gmail.com>
2026-07-30 21:07:33 +0530

    Fix API base URL typo"Fix backend API URL"

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://mern-job-portal-api-dihw.onrender.com/api",
+  baseURL: "https://mern-job-portal-api-dhiw.onrender.com/api",
 });
 
 export default API;
